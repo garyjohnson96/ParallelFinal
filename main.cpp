31aabcd8fde0007ba759a334ee26b796284db3f4
Licked Wizard <tylerlott0126@gmail.com>
2021-04-10 22:52:42 -0600

    finished and loaded a test case that I have previous answers for, the result from our program is correct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <mpi.h>
 #include <chrono>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,48 +14,44 @@ using namespace std;
 #define TERMINATE 2
 
 // globals for different size meshes
-const int TINY = 10;
+const int TINY = 6;
 const int SMALL = 100;
 const int MEDIUM = 1000;
 const int LARGE = 10000;
 const int VERY_LARGE = 100000;
 
+const char * getColor(float temp){
+    if (temp < 25) { return "\033[1;34m"; }
+    if (temp >= 25 && temp <= 60) { return "\033[1;33m"; }
+    if (temp > 60) { return "\033[1;31m"; }
+    return "\033[0m";
+}
+
 // DEBUG helper function to check the values in the mesh
-void printMesh(vector<vector<int>> mesh, int rank) {  
+void printMesh(vector<vector<float>> mesh, int rank) {
+    cout << string(2,'\n');
     cout << "Rank: " << rank << endl;
     for (size_t row = 0; row < mesh.size(); row++) {
-        for (size_t col = 0; col < mesh[row].size(); col++) { 
-            cout << mesh[row][col] << " ";
+        for (size_t col = 0; col < mesh[row].size(); col++) {
+            const char* color = getColor(mesh[row][col]);
+            cout << std::fixed << std::setprecision(4) << color << mesh[row][col] << " " << "\033[0m";
         }
         cout << endl;
     }
 }
 
 // Sets the hot and cool constant temps to random cells
-void setTemps(vector<vector<int>>& mesh, int rank, int hotTemp, int coolTemp) {
-    int constCells = mesh.size() * 0.2;   // 20% of the mesh cells are constant temperature points
-
-    vector<vector<int>> hotCells(constCells, vector<int>(2));  // mesh coordinates of the hot and cool cells
-    vector<vector<int>> coolCells(constCells, vector<int>(2));
+void setTemps(vector<vector<float>>& mesh, vector<vector<int>> hotCells, vector<vector<int>> coolCells, int rank, float hotTemp, float coolTemp) {
 
     // set the hot and cool cell coordinates
-    for (int cell = 0; cell < constCells; cell++) {
-        if (rank == MASTER) {
-            hotCells[cell][0] = rand() % mesh.size();
-            hotCells[cell][1] = rand() % mesh.size();
-
-            do {  // Ensures the hot and cool cells aren't the same
-                coolCells[cell][0] = rand() % mesh.size();
-                coolCells[cell][1] = rand() % mesh.size();
-            } while (coolCells[cell][0] == hotCells[cell][0] && coolCells[cell][1] == hotCells[cell][1]);
-        }
+    for (int cell = 0; cell < hotCells.size(); cell++) {
 
         MPI_Bcast(hotCells[cell].data(), hotCells[cell].size(), MPI_INT, MASTER, MCW);
         MPI_Bcast(coolCells[cell].data(), coolCells[cell].size(), MPI_INT, MASTER, MCW);
-        
+
         // Set constant temperature cells accordingly
-        mesh[hotCells[cell][0]][hotCells[cell][1]] = hotTemp; 
-        mesh[coolCells[cell][0]][coolCells[cell][1]] = coolTemp; 
+        mesh[hotCells[cell][0]][hotCells[cell][1]] = hotTemp;
+        mesh[coolCells[cell][0]][coolCells[cell][1]] = coolTemp;
     }
 }
 
@@ -63,23 +61,29 @@ int main(int argc, char** argv) {
     int rank, size;
     MPI_Comm_rank(MCW, &rank);
     MPI_Comm_size(MCW, &size);
+    float dt = 1.5, dx = 0.038, therm_diff = 0.000111; // dt-time step, dx-node separation, thermal diffusivity is for copper
+    float fourier = (dt * therm_diff) / (dx * dx); // FOURIER MUST BE < 0.5 FOR STABILITY
+    float hot = 75, cold = 20;
 
     srand(chrono::system_clock::now().time_since_epoch().count());  // seed rand() by current time
 
-    vector<vector<int>> mesh(TINY, vector<int>(TINY, 25));  // initialize the mesh with the base temperature
+    vector<vector<float>> mesh(TINY, vector<float>(TINY, 25));  // initialize the mesh with the base temperature
 
-    setTemps(mesh, rank, 75, 20);
+    vector<vector<int>> hotCells{ {3, 0} , {4, 2} };  // mesh coordinates of the hot and cool cells
+    vector<vector<int>> coolCells{ {1, 3} , {4, 4} };
+
+
+    setTemps(mesh, hotCells, coolCells, rank, hot, cold);
     MPI_Barrier(MCW);
-    
-    int iterations = 10;  // change maybe?
+
+    int iterations = 200;  // change maybe?
     int timeStep = 0;
     while (timeStep < iterations) {
 
-        printMesh(mesh, rank);
-
         if (rank == MASTER) {
+            printMesh(mesh, rank);
             int currentRow = 0;
-            int workingProcesses = 0; 
+            int workingProcesses = 0;
 
             // Begin sending new work to each worker
             for (int worker = 1; worker < size; worker++) {
@@ -87,15 +91,15 @@ int main(int argc, char** argv) {
                 MPI_Send(&currentRow, 1, MPI_INT, worker, WORK, MCW);
                 currentRow++;
                 workingProcesses++;
-            } 
-            
+            }
+
             int rows = mesh.size();
             MPI_Status workerStatus;
 
             // Send rows until all rows have been completed
             while (workingProcesses > 0) {
-                vector<int> receivedRow(mesh[0].size());
-                
+                vector<float> receivedRow(mesh[0].size());
+
                 // Receive row of work from worker. The row index is received as the tag
                 MPI_Recv(receivedRow.data(), receivedRow.size(), MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &workerStatus);
                 workingProcesses--;
@@ -117,30 +121,55 @@ int main(int argc, char** argv) {
             bool working = true;
             MPI_Status workStatus;
             while (working) {
-                // receives index. 
+                // receives index.
                 MPI_Recv(&row, 1, MPI_INT, MASTER, MPI_ANY_TAG, MCW, &workStatus);
                 if (workStatus.MPI_TAG == TERMINATE) {
                     working = false;
                 } else {
-                    vector<int> updatedRow(mesh[0].size());
+                    vector<float> updatedRow(mesh[0].size());
+                    float a, b, c, d;
 
-                    /*** update row ***/
+                    for (int i=0; i<mesh[0].size(); i++) {
+                        vector<int> key = {row, i};
+                        if ((std::find(hotCells.begin(), hotCells.end(), key) != hotCells.end()) ||
+                             (std::find(coolCells.begin(), coolCells.end(), key) != coolCells.end())) { // check to see if a constant cell
+                            updatedRow[i] = mesh[row][i];
+                        } else {
+	                        if (row == 0) { // boundary conditions at edges is insulated meaning we use the inner point twice
+	                            a = mesh[row+1][i];
+	                        } else {
+	                            a = mesh[row-1][i]; }
+	                        if (row == mesh[0].size()-1) {
+	                            b = mesh[row-1][i];
+	                        } else {
+	                            b = mesh[row+1][i]; }
+	                        if (i == 0) {
+	                            c = mesh[row][i+1];
+	                        } else {
+	                            c = mesh[row][i-1]; }
+	                        if (i == mesh[0].size()-1) {
+	                            d = mesh[row][i-1];
+	                        } else {
+	                            d = mesh[row][i+1]; }
+	                        updatedRow[i] = fourier * (a + b + c + d) + (1 - 4 * fourier) * mesh[row][i];
+                        }
+                    }
 
                     // Send updated row back to master with the row index as the tag
-                    MPI_Send(updatedRow.data(), updatedRow.size(), MPI_INT, MASTER, row, MCW);
+                    MPI_Send(updatedRow.data(), updatedRow.size(), MPI_FLOAT, MASTER, row, MCW);
                 }
             }
-        }    
-        
+        }
+
         timeStep++;
-        
+
         // Update each process's mesh
         for (int row = 0; row < mesh.size(); row++) {
-            MPI_Bcast(mesh[row].data(), mesh[row].size(), MPI_INT, MASTER, MCW);
+            MPI_Bcast(mesh[row].data(), mesh[row].size(), MPI_FLOAT, MASTER, MCW);
             MPI_Barrier(MCW);
         }
     }
-    
+
     MPI_Finalize();
     return 0;
 }
