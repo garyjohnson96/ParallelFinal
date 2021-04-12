2c3358ac7df8ef798b43110a3c199c94c81746f2
Licked Wizard <tylerlott0126@gmail.com>
2021-04-12 15:19:35 -0600

    Added command line inputs for variables, added runtime printout, polished a few other things.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,6 @@ using namespace std;
 #define WORK 1
 #define TERMINATE 2
 
-// globals for different size meshes
-const int TINY = 6;
-const int SMALL = 100;
-const int MEDIUM = 1000;
-const int LARGE = 10000;
-const int VERY_LARGE = 100000;
-
 const char * getColor(float temp){
     if (temp < 25) { return "\033[1;34m"; }
     if (temp >= 25 && temp <= 60) { return "\033[1;33m"; }
@@ -28,9 +21,9 @@ const char * getColor(float temp){
 }
 
 // DEBUG helper function to check the values in the mesh
-void printMesh(vector<vector<float>> mesh, int rank) {
+void printMesh(vector<vector<float>> mesh, int rank, float timestep) {
     cout << string(2,'\n');
-    cout << "Rank: " << rank << endl;
+    cout << "Rank: " << rank << " Time: "<<timestep<<endl;
     for (size_t row = 0; row < mesh.size(); row++) {
         for (size_t col = 0; col < mesh[row].size(); col++) {
             const char* color = getColor(mesh[row][col]);
@@ -58,30 +51,44 @@ void setTemps(vector<vector<float>>& mesh, vector<vector<int>> hotCells, vector<
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
+    double start, end;
     int rank, size;
     MPI_Comm_rank(MCW, &rank);
     MPI_Comm_size(MCW, &size);
-    float dt = 1.5, dx = 0.038, therm_diff = 0.000111; // dt-time step, dx-node separation, thermal diffusivity is for copper
-    float fourier = (dt * therm_diff) / (dx * dx); // FOURIER MUST BE < 0.5 FOR STABILITY
+    // take in the command line variables
+    int mesh_size, iterations;
+    sscanf(argv[1], "%d", &mesh_size);
+    sscanf(argv[5], "%d", &iterations);
+    float dt, dx, therm_diff;
+    sscanf(argv[2],"%f", &dt);
+    sscanf(argv[3],"%f", &dx);
+    sscanf(argv[4],"%f", &therm_diff);
+//    float dt = 1.5, dx = 0.038, therm_diff = 0.000111; // dt-time step, dx-node separation, thermal diffusivity is for copper
+    float fourier = (dt * therm_diff) / (dx * dx); // FOURIER MUST BE < 0.25 FOR STABILITY
+    if (fourier > 0.25) {
+        if (rank==MASTER) { cout<<"Fourier of "<<fourier<<" is larger than 0.25 reduce timestep for stability, exiting..."<<endl; }
+        MPI_Finalize();
+        return 0;
+    }
     float hot = 75, cold = 20;
 
     srand(chrono::system_clock::now().time_since_epoch().count());  // seed rand() by current time
 
-    vector<vector<float>> mesh(TINY, vector<float>(TINY, 25));  // initialize the mesh with the base temperature
+    vector<vector<float>> mesh(mesh_size, vector<float>(mesh_size, 25));  // initialize the mesh with the base temperature
 
     vector<vector<int>> hotCells{ {3, 0} , {4, 2} };  // mesh coordinates of the hot and cool cells
     vector<vector<int>> coolCells{ {1, 3} , {4, 4} };
 
-
     setTemps(mesh, hotCells, coolCells, rank, hot, cold);
     MPI_Barrier(MCW);
 
-    int iterations = 200;  // change maybe?
+    start = MPI_Wtime();
+
     int timeStep = 0;
     while (timeStep < iterations) {
 
         if (rank == MASTER) {
-            printMesh(mesh, rank);
+            printMesh(mesh, rank, timeStep*dt);
             int currentRow = 0;
             int workingProcesses = 0;
 
@@ -171,5 +178,9 @@ int main(int argc, char** argv) {
     }
 
     MPI_Finalize();
+
+    end = MPI_Wtime();
+    if (rank==MASTER) { printMesh(mesh,rank, iterations*dt); cout<<"Runtime = "<<end-start<<endl; }
+
     return 0;
 }
